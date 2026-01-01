987ae6a41dea0262a5ec0a71b9e6b327b5c55891
dallinnd <156145316+dallinnd@users.noreply.github.com>
2026-01-01 13:54:17 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,24 @@
-const CACHE_NAME = 'panda-royale-v1';
+Service Worker (sw.js)
+```
+const CACHE_NAME = 'panda-v5';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
-  'https://cdn.tailwindcss.com'
+  './icon-192.png',
+  './icon-512.png'
 ];
-
-// Install Service Worker
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
 
-// Activate & Clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-// Fetch from cache first, then network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
+```
